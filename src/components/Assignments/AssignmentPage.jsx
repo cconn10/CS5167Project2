fadc63e1b0397f376d0c06b8286f62072bad72e5
Veronica Ufferman <55750257+uffermvg@users.noreply.github.com>
2022-11-15 14:33:25 -0500

    Add files via upload

--- src/components/Assignments/AssignmentPage.jsx
+++ src/components/Assignments/AssignmentPage.jsx
@@ -1,47 +1,21 @@
 import React, { Component } from 'react';
 import Grades from './Grades';
-import Filter from './Filter';
+import Assignments from './Assignments';
 import './AssignmentPage.css';
-//import data from './data/metadata.json';
 
 class AssignmentPage extends Component {
-    state = {
-        
-    };
-
-
+    
     render() { 
         return (
-            <React.Fragment>
-                <div className="gridContainer">
+            <div className="gridContainer">
                 <div className="classGrades">
-                <Grades></Grades>
+                    <Assignments></Assignments>
+                    <Grades></Grades>
                 </div>
-                <div className="filter">
-                    <Filter></Filter>
-                </div>
-                </div>
-            </React.Fragment>
+            </div>
+
         );
     }
-
-    /*getUIGradesData (){
-        var UIassignments = data.ui.filter( element => element.type === "assignment");
-        var UIgrades;
-
-        var date = new Date();
-        for (let i = 0; i < UIassignments.ui.length; i++) {
-            const element = UIassignments.ui[i];
-            const due = new Date(element.end_or_due);
-            
-            if(due.getTime() < date.getTime()){
-                UIgrades = UIgrades + UIassignments.UI;
-            }
-        }
-
-        return  UIgrades;
-
-    }; */
 };
  
 export default AssignmentPage;
---
+++ src/components/Assignments/UIAssignments.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import JsonData from './data/uiAssignments.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faDownload, faUpload} from '@fortawesome/free-solid-svg-icons';
+
+function JsonDataDisplay(){
+    const DisplayData = JsonData.assignments.map(
+        (info)=>{
+            return(
+                <tr>
+                    <td>{info.title}</td>
+                    <td>{<button type="button" class="btn btn-light" ><FontAwesomeIcon className='icon-asignment' icon={ faDownload } /></button>}</td>
+                    <td>{<button type="button" class="btn btn-light" ><FontAwesomeIcon className='icon-assignment' icon={ faUpload } /></button>}</td>
+                    <td>{info.end_or_due}</td>
+                    <td>{info.points}</td>
+                </tr>
+            )
+        }
+    )
+
+    return(
+        <div>
+            <div id="accordion">
+                <div class="card">
+                    <div class="card-header bg-info text-white" id="headingOne">
+                        <h5 class="mb-0">
+                            USER INTERFACE 1: Assignments
+                         </h5>
+                    </div>
+
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                        <div class="card-body">
+                        <table class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Assignment</th>
+                                            <th scope="col">Download</th>
+                                            <th scope="col">Submit</th>
+                                            <th scope="col">Due Date</th>
+                                            <th scope="col">Points</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                    {DisplayData}
+                </tbody>
+            </table>
+            </div>
+                    </div>
+                </div>
+             </div>
+        </div>
+    )
+}
+
+export default JsonDataDisplay;

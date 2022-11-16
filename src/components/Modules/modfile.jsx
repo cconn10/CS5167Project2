513a0510853b11e8c8e85d9c8586004379a9ae7b
Jenn <97474493+jenngutman4@users.noreply.github.com>
2022-11-16 15:55:57 -0500

    update modal

--- src/components/Modules/modfile.jsx
+++ src/components/Modules/modfile.jsx
@@ -5,35 +5,69 @@ import './modfile.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faDownload, faUpload} from '@fortawesome/free-solid-svg-icons';
 
-import Modal from './Modal';
+import Modal from '../Modal';
+import '../Home/ToDoList.css';
 
 
 
 class ModFile extends Component {
+    // state = {
+    //     seen: false
+    // };
+    // togglePop = () => {
+    // this.setState({
+    //     seen: !this.state.seen
+    // });
+    // console.log(this.state.seen);
+    // };
     state = {
-        seen: false
+        show: false
+      };
+    showModal = e => {
+        this.setState({
+          show: !this.state.show
+        });
+        console.log("state: ");
+        console.log(this.state.show);
     };
-    togglePop = () => {
-    this.setState({
-        seen: !this.state.seen
-    });
-    console.log(this.state.seen);
-    };
+    
 
     render() {
         const {file} = this.props;
         
         console.log(file);
         return (
-            <div>
+            <React.Fragment>
                 <span>{file.title}</span>
-                <button type="button" class="btn btn-light" ><FontAwesomeIcon className='icon-asignment' icon={ faDownload } /></button>
-                {/* <Modal /> */}
+                <button type="button" class="btn btn-light" onClick={e => {this.showModal();}}><FontAwesomeIcon className='icon-asignment' icon={ faDownload } /></button>
+                <Modal onClose={this.showModal} show={this.state.show}>
+                        Message in Modal
+                </Modal>
                 {/* <div className="btn" onClick={this.togglePop}>
                     <button>New User?</button>
                 </div>
                 {this.state.seen ? <PopUp toggle={this.togglePop} /> : null} */}
-            </div>
+                {/* <div className="modal" id="modal" tabIndex="-1">
+                            <div className="modal-dialog modal-dialog-centered">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                    <div className='col'>
+                                        <div className='row'>
+                                            <h5 className="modal-title">Title</h5>
+                                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div className='row'>
+                                            <p className='h6'>Due Date:</p>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <div className="modal-body">
+                                    </div>
+                                </div>
+                            </div>
+                </div> */}
+                        
+            </React.Fragment>
         );
     }
 }

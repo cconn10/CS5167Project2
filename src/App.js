aca54373c9b8d4dab1bdfd8dbe53ca29a0152d0f
Colin Conn <conncf@mail.uc.edu>
2022-11-16 17:55:23 -0500

    Calendar (Not working yet)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react';
 import './App.css';
 import './scss/custom.scss'
 
+import CanvasCalendar from './components/Calendar/Calendar'
 import Navbar from './components/NavBar/Navbar';
 import HomePage from './components/Home/HomePage';
 import AssignmentPage from "./components/Assignments/AssignmentPage";
@@ -18,6 +19,7 @@ class App extends Component {
           <Routes>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/Assignments" element={<AssignmentPage />}></Route>
+            <Route path="/Calendar" element={<CanvasCalendar />}></Route>
           </Routes>
         </BrowserRouter>
       </div>
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -22,7 +22,7 @@ class Navbar extends Component {
                 <a className='nav-link' href="/Assignments" data-toggle="tooltip" data-bs-placement="bottom" title="Grades" >
                     <FontAwesomeIcon className='icon mx-2' icon={ faClipboard } />
                 </a>
-                <a className='nav-link' href="/" data-toggle="tooltip" data-bs-placement="bottom" title="Calendar" >
+                <a className='nav-link' href="/Calendar" data-toggle="tooltip" data-bs-placement="bottom" title="Calendar" >
                     <FontAwesomeIcon className='icon mx-2' icon={ faCalendar }/>
                 </a>
             </nav>

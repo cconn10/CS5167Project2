edcc154f63d10f28193f698cb00ebf4335de5593
Colin Conn <conncf@mail.uc.edu>
2022-11-16 11:02:36 -0500

    ToDoList as ul

--- src/components/Home/ToDoList.jsx
+++ src/components/Home/ToDoList.jsx
@@ -12,10 +12,74 @@ class ToDoList extends Component {
 
     getId = name => name.toLowerCase().split("").join("-")
 
+    listItems(items, courseName) {
+        return (
+            <div>
+                { items.map((item, index) => (
+                    <div key={index}>
+                        <li className="list-group-item">
+                            <div className='row h6'>
+                                <div className='col-5 '>
+                                    {item.title}
+                                </div>
+                                <div className='col'>
+                                    <span className={'badge ' + this.getCourseColor(courseName)}>{ courseName }</span>
+                                </div>
+                            </div>
+                            <div className='row'>
+                                <div className='col'>
+                                    Due Date: {item.end_or_due}
+                                </div>
+                                <div className='col'>
+                                    
+                                </div>
+                            </div>
+                        </li>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+
+    getCourseColor(name){
+        switch(name){
+            case "User Interface":
+                return "bg-info"
+            case "Senior Design":
+                return "bg-success"
+            case "Computer Graphics":
+                return "bg-warning"
+            default:
+                return "bg-primary"
+        }
+    }
+
+    renderLists(e){
+        this.setState({filterOption: e.target.value})
+    }
+
     render() { 
         const courseInfo = this.props.courseInfo
-        const toDoItems = this.props.toDoItems
+        const uiItems = this.props.uiToDoItems
+        const seniorDesignItems = this.props.seniorDesignToDoItems
+        const graphicsItems = this.props.computerGraphicsToDoItems
+        var listItems = <div></div>
 
+        if(this.state.filterOption === "User Interface")
+            listItems = this.listItems(uiItems, "User Interface")
+        else if (this.state.filterOption === "Senior Design")
+            listItems = this.listItems(seniorDesignItems, "Senior Design")
+        else if (this.state.filterOption === "Computer Graphics")
+            listItems = this.listItems(graphicsItems, "Computer Graphics")
+        else
+            listItems = <div>
+                { this.listItems(uiItems, "User Interface") }
+                { this.listItems(seniorDesignItems, "Senior Design") }
+                { this.listItems(graphicsItems, "Computer Graphics") }
+            </div>
+
+        this.renderLists = this.renderLists.bind(this);
+        
         return (
             <React.Fragment>
                 <div className='container border rounded toDoList'>
@@ -24,7 +88,7 @@ class ToDoList extends Component {
                             <h3>To Do List</h3>
                         </div>
                         <div className='col'>
-                            <select className="form-select" defaultValue={this.state.filterOption} onChange={this.filterToDo()} aria-label="Default select example">
+                            <select className="form-select" defaultValue={this.state.filterOption} onChange={this.renderLists} aria-label="Default select example">
                                 <option value={"All Courses"}>All Courses</option>
                                 { courseInfo.courses.map(course => (
                                     <option key={course.name} value={course.name}>{ course.name }</option>
@@ -32,9 +96,9 @@ class ToDoList extends Component {
                             </select>
                         </div>
                     </div>
-                    { toDoItems.map((item, index) => (
-                        <h6 className="header" key={index}>{ item.title }</h6>
-                    ))}
+                    <ul className='list-group list-group-flush'>
+                        { listItems }
+                    </ul>
                 </div>
             </React.Fragment>
         );
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser, faBook, faClipboard, faHome} from '@fortawesome/free-solid-svg-icons';
 import { faCalendar } from '@fortawesome/free-regular-svg-icons';
+
 import './Navbar.css'
 
 class Navbar extends Component {
@@ -10,19 +11,19 @@ class Navbar extends Component {
         return (
         <React.Fragment>
             <nav className="navbar nav bg-primary d-flex flex-row justify-content-start">
-                <a className='nav-link' href="/" data-toggle="tooltip" data-bs-placement="bottom" title="Home" >
+                <a className='nav-link' href="/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home" >
                     <FontAwesomeIcon className='icon mx-2' icon={ faHome } />
                 </a>
-                <a className='nav-link' href="/" data-toggle="tooltip" data-bs-placement="bottom" title="Profile" >
+                <a className='nav-link' href="/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Profile" >
                     <FontAwesomeIcon className='icon mx-2' icon={ faCircleUser } />
                 </a>
-                <a className='nav-link' href="/" data-toggle="tooltip" data-bs-placement="bottom" title="Courses" >
+                <a className='nav-link' href="/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Courses" >
                     <FontAwesomeIcon className='icon mx-2' icon={ faBook } />
                 </a>
-                <a className='nav-link' href="/Assignments" data-toggle="tooltip" data-bs-placement="bottom" title="Grades" >
+                <a className='nav-link' href="/Assignments" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Grades" >
                     <FontAwesomeIcon className='icon mx-2' icon={ faClipboard } />
                 </a>
-                <a className='nav-link' href="/" data-toggle="tooltip" data-bs-placement="bottom" title="Calendar" >
+                <a className='nav-link' href="/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Calendar" >
                     <FontAwesomeIcon className='icon mx-2' icon={ faCalendar }/>
                 </a>
             </nav>

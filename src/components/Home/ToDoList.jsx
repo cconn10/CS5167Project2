e2f04b2795dab68de4b488ab77f0d179e97acc7a
Colin Conn <conncf@mail.uc.edu>
2022-11-16 14:50:49 -0500

    Modals

--- src/components/Home/ToDoList.jsx
+++ src/components/Home/ToDoList.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+
+import Modal from '../Modal';
+
 import './ToDoList.css'
 
 class ToDoList extends Component {
     state = { 
         filterOption: "All Courses",
-     } 
-
-    filterToDo() {
-
+        subItems: {}
     }
 
     getId = name => name.toLowerCase().split("").join("-")
@@ -19,22 +19,82 @@ class ToDoList extends Component {
                     <div key={index}>
                         <li className="list-group-item">
                             <div className='row h6'>
-                                <div className='col-5 '>
-                                    {item.title}
-                                </div>
-                                <div className='col'>
-                                    <span className={'badge ' + this.getCourseColor(courseName)}>{ courseName }</span>
-                                </div>
+                                {item.title}
+                            </div>
+                            <div className='row-2 mb-1'>
+                                <span className={'badge ' + this.getCourseColor(courseName)}>{ courseName }</span>
                             </div>
                             <div className='row'>
                                 <div className='col'>
                                     Due Date: {item.end_or_due}
                                 </div>
                                 <div className='col'>
-                                    
+                                    <button className='btn btn-primary detailsButton' data-bs-toggle="modal" data-bs-target='#modal'>
+                                        Details
+                                    </button>
                                 </div>
                             </div>
                         </li>
+                        <div className="modal" id="modal" tabIndex="-1">
+                            <div className="modal-dialog modal-dialog-centered">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                    <div className='col'>
+                                        <div className='row'>
+                                            <h5 className="modal-title">{item.title}</h5>
+                                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div className='row'>
+                                            <p className='h6'>Due Date: {item.end_or_due}</p>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <div className="modal-body">
+                                        <p className='h5'>Goal Tracking</p>
+                                        <ul className='form-check list-unstyled'>
+                                            <li>
+                                                <input className="form-check-input" type="checkbox" value="" />
+                                                <label className="form-check-label">
+                                                    Goal #1
+                                                </label>
+                                                <ul>
+                                                    <li>
+                                                        <input className="form-check-input" type="checkbox" value="" />
+                                                        <label className="form-check-label">
+                                                            Subgoal #1
+                                                        </label>
+                                                    </li>
+                                                    <li>
+                                                        <input className="form-check-input" type="checkbox" value="" />
+                                                        <label className="form-check-label">
+                                                            Subgoal #2
+                                                        </label>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li>
+                                                <input className="form-check-input" type="checkbox" value="" />
+                                                <label className="form-check-label">
+                                                    Goal #2
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <input className="form-check-input" type="checkbox" value=""/>
+                                                <label className="form-check-label">
+                                                    Goal #3
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <input className="form-check-input" type="checkbox" value=""/>
+                                                <label className="form-check-label">
+                                                    Goal #4
+                                                </label>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
@@ -63,7 +123,7 @@ class ToDoList extends Component {
         const uiItems = this.props.uiToDoItems
         const seniorDesignItems = this.props.seniorDesignToDoItems
         const graphicsItems = this.props.computerGraphicsToDoItems
-        var listItems = <div></div>
+        let listItems = <div></div>
 
         if(this.state.filterOption === "User Interface")
             listItems = this.listItems(uiItems, "User Interface")
@@ -96,7 +156,7 @@ class ToDoList extends Component {
                             </select>
                         </div>
                     </div>
-                    <ul className='list-group list-group-flush'>
+                    <ul className='list-group list-group-flush toDoListItems'>
                         { listItems }
                     </ul>
                 </div>
